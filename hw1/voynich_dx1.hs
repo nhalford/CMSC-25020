257324bb602225434de67b8aa4e7beb734d5b08a
Noah Halford <noah.halford@gmail.com>
2015-04-05 19:28:17 -0500

    Updated .hs file and compiled binary

--- hw1/voynich_dx1.hs
+++ hw1/voynich_dx1.hs
@@ -1,36 +1,56 @@
-{-
- - A list of important functions:
- - extractSource :: String -> Char
- --- Finds source from typical format angle brackets
- - lineType :: String -> Maybe Line
- --- determines if a line is a comment or a transcription
- --- (or neither)
- - readComment :: String -> Maybe (Char, String)
- --- Returns Just (language, hand) if the comment contains
- --- that information and Nothing otherwise
- - readTranscription :: String -> Maybe (String, String)
- --- splits a transcription line into (info, words)
- - filename :: (Char, String, Char) -> String
- --- create dx1 file name from (lang, hand, source)
- - updateMap :: (Ord k, Num a) => M.Map k a -> k -> M.Map k a
- --- given a word, increases the count of that word in the map by 1
- - updateMapFromLine :: M.Map String Int -> String -> M.Map String Int
- --- given a sequence of words, updates the map with their appropriate
- --- counts
- - exportMap :: (Show a, Ord a) => String -> M.Map String a -> IO ()
- --- writes map to file given by the string
- -}
-
 import Data.Char
 import Data.List
+import Data.Maybe
 import qualified Data.Map.Strict as M 
 import System.IO
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State.Lazy
+
+-- Some type synonyms
+type Multimap = M.Map String (M.Map String Int)
+type MapTuple = (Char, String, Multimap)
 
 main :: IO ()
 main = do
+    evalStateT setup (' ',"",filemaps)
     content <- readFile "interlin.txt"
     let fileLines = lines content
-    mapM_ print fileLines
+    (_,_,maps) <- execList (map processLine fileLines) ('A',"1",filemaps)
+    let goodmaps = filter (\(k,v) -> v /= M.empty) (M.toList maps)
+    mapM_ exportMap goodmaps
+    return ()
+
+-- Initial state setup
+setup :: StateT MapTuple IO ()
+setup = do
+    put (' ',"",filemaps)
+    return ()
+
+-- Function to execute a list of States in sequence
+execList :: [StateT MapTuple IO ()] -> MapTuple -> IO MapTuple
+execList [] st = return st
+execList (x:xs) st = do
+    result <- execStateT x st
+    new <- execList xs result
+    return new
+
+-- Function for processing a line of text
+processLine :: String -> StateT MapTuple IO ()
+processLine line = do
+    (l,h,ms) <- get
+    let lt = lineType line
+    let readC = readComment line
+    let (newL, newH) = if readC /= Nothing then fromJust readC else (' ',"")
+    case lt of Nothing -> return ()
+               Just Comment -> if readC /= Nothing then put (newL,newH,ms) else return ()
+               Just Transcription -> do
+                    let (info, trans) = case readTranscription line of Just (x,y) -> (x, y)
+                                                                       Nothing -> ("","")
+                    if (info, trans) /= ("","") then do
+                        let ms' = updateMap (l, h) (info, trans) ms
+                        put (l, h, ms')
+                    else return ()
     return ()
 
 -- function to remove characters from a string
@@ -46,12 +66,28 @@ removeChars cs = filter (not . (flip elem cs))
 -- there are no nested comments, i.e., anything
 -- of the form c1 xxxx c1 xxxx c2 xxxx c2
 removeBetween :: Char -> Char -> String -> String
-removeBetween _ _ [] = []
+removeBetween _ _ "" = ""
 removeBetween c1 c2 s
     | not ((c1 `elem` s) && (c2 `elem` s)) = s
     | otherwise = beginning ++ (removeBetween c1 c2 (tail end))
     where (beginning, rest) = break (== c1) s
           (_, end) = break (== c2) rest
+
+-- this is a helper function used to deal with the
+-- instances when we have a word break in between
+-- square brackets. It replaces any instance of a period
+-- in square brackets with a +, and we will then change
+-- this + back to a period later. This allows us to treat
+-- anything with brackets as one word even if there are
+-- periods (i.e. spaces) within the brackets.
+correctBrackets :: String -> String
+correctBrackets "" = ""
+correctBrackets s
+    | not (('[' `elem` s) && (']' `elem` s)) = s
+    | otherwise = beginning ++ (fix inBrackets) ++ (correctBrackets end)
+    where (beginning, rest) = break (== '[') s
+          (inBrackets, end) = break (== ']') rest
+          fix t = map (\x -> if (x == '.') then '+' else x) t
 
 -- similar to words from Prelude, but splits on a
 -- a specified character rather than on whitespace
@@ -84,15 +120,17 @@ lineType line
 -- will be given by an empty string, i.e., ""
 -- A comment of the form "# Currier's language A, hand 1"
 -- will evalueate to Just ('A',"1"). Comments with no
--- specified language will evaluate to Nothing
+-- specified language will evaluate to Nothing.
+-- If a language but no hand is specified, then we
+-- take the hand to be hand 0.
 readComment :: String -> Maybe (Char, String)
 readComment s
     | "language" `isInfixOf` (map toLower s) = Just (language, hand)
     | otherwise = Nothing
-    where language = toUpper $ last $ head splitString
+    where language = toUpper $ last $ filter (/= '\r') $ head splitString
           splitString = splitOnChar ',' s
-          hand = case splitString of [] -> ""
-                                     [x] -> ""
+          hand = case splitString of [] -> "0"
+                                     [x] -> "0"
                                      [x,y] -> last $ words y
 
 -- function to read a transcription line
@@ -102,7 +140,9 @@ readComment s
 readTranscription :: String -> Maybe (String, String)
 readTranscription line = case (words line) of [] -> Nothing
                                               [x] -> Nothing
-                                              (x:xs) -> Just (x, takeWhile (/= '-') $ concat xs)
+                                              (x:xs) -> if (';' `elem` x) then
+                                                    Just (x, takeWhile (\x -> (x /= '-') && (x /= '=')) $ concat xs)
+                                              else Nothing
 
 -- Takes (lang, hand, source), converts to name of
 -- output dx1 file
@@ -119,7 +159,7 @@ sources = ['C','F','T','L','R','K','J']
 
 -- list of all hands
 hands :: [String]
-hands = ["","1","2","3","4","4?","5","X","Y"]
+hands = ["0","1","2","3","4","4?","5","X","Y"]
 
 -- a list of all possible files, i.e., all possible combinations
 -- of language/hand/source. Not all will be used.
@@ -128,13 +168,14 @@ filenames = [filename (l, h, s) | l <- languages, h <- hands, s <- sources]
 
 -- map from file names to their corresponding (word, count) maps
 -- starts empty but will update as we read the file
-filemaps :: M.Map String (M.Map String Int)
+filemaps :: Multimap
 filemaps = M.fromList $ zip filenames (repeat M.empty)
+--filemaps = M.insert "test.dx1" (updateMapFromLine M.empty "this.is.a.test.yes.this.is") (M.fromList $ zip filenames (repeat M.empty))
 
 -- Update a map given a word. Either increase the count of that
 -- word, or set it to 1 if it is not in the map already.
-updateMap :: (Ord k, Num a) => M.Map k a -> k -> M.Map k a
-updateMap m word = do
+updateFromWord :: (Ord k, Num a) => M.Map k a -> k -> M.Map k a
+updateFromWord m word = do
     let value = M.lookup word m
     case value of Nothing -> M.insert word 1 m
                   Just x -> M.adjust (+1) word m
@@ -143,8 +184,16 @@ updateMap m word = do
 -- in line. This expects the second part of the line, i.e.,
 -- only the transcription (not the things in angle brackets)
 updateMapFromLine :: M.Map String Int -> String -> M.Map String Int
-updateMapFromLine m line = foldl updateMap m ws
-    where ws = splitOnChar '.' $ removeChars ['!','%'] (removeBetween '{' '}' line)
+updateMapFromLine m line = foldl updateFromWord m ws'
+    where ws' = map (map (\x -> if x == '+' then '.' else x)) ws
+          ws = splitOnChar '.' $ removeChars ['!','%'] (removeBetween '{' '}' $ correctBrackets line)
+
+-- Takes (lang, hand) and (<info>, transcription), then
+-- updates the appropriate map from the map of maps it is given
+updateMap :: (Char, String) -> (String, String) -> M.Map String (M.Map String Int) -> M.Map String (M.Map String Int)
+updateMap (l,h) (info, trans) ms = M.insert key (updateMapFromLine m trans) ms
+    where m = fromJust $ M.lookup key ms
+          key = filename (l,h, extractSource info)
 
 -- helper function for sortMap: sorts first by b (corresponding
 -- to counts), then by a. Note that we compare in the opposite
@@ -163,12 +212,14 @@ sortMap = sortBy comparePair . M.toList
 
 -- write map m to file at path, sorted first by count
 -- and then alphabetically
-exportMap :: (Show a, Ord a) => String -> M.Map String a -> IO ()
-exportMap path m = writeFile path (concat strings)
+exportMap :: (Show a, Ord a) => (String, M.Map String a) -> IO ()
+exportMap (path, m) = writeFile ("output/" ++ path) (concat (comments : strings))
     where strings = map (\(k,v) -> k ++ "\t" ++ (show v) ++ "\n") (sortMap m)
-
-{- TODO:
- - update (l, h) state
- - IO
- - include notes on choices
--}
+          comments = unlines ["# It is unclear exactly what asterisks (*) in this file mean"
+                             ,"# so they were treated as part of the alphabet."
+                             ,"# Comments, exclamation points (!), and percent"
+                             ,"# symbols (%) were removed."
+                             ,"# Varations (of the form [A|B]) were not treated"
+                             ,"# specially. That is, anything of the form xxx[xxx|xxx]xxx"
+                             ,"# was treated as one word, even if there were periods,"
+                             ,"# i.e., spaces, within the brackets."]
